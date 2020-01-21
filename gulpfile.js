88029597d429eb1c37a2e254fdb944d9d28fcded
Livanenkova <arina909@yandex.ru>
2020-01-21 18:17:55 +0300

    dz 9/1

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,9 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
-var htmlmin = require("gulp-htmlmin");
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -68,6 +70,14 @@ gulp.task("minify", function() {
   return gulp.src("source/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("compress", function () {
+  return pipeline(
+        gulp.src('source/js/*.js'),
+        uglify(),
+        gulp.dest('build/js')
+  );
 });
 
 gulp.task("copy", function () {
